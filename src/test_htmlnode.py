6b3833c0862b647c89b5b22e20a88f301a705aed
CarlKarlQarl <karlmatthes@gmail.com>
2026-06-29 11:14:39 -0400

    added leafnode and tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,7 +1,8 @@
 import unittest
 from htmlnode import HTMLNode
+from htmlnode import LeafNode
 
-class TestTextNode(unittest.TestCase):
+class TestHTMLNode(unittest.TestCase):
     def test_eq1(self):
         node = HTMLNode(tag="p", value="This is a text node", children=[], props={"class": "text"})
         node2 = HTMLNode(tag="p", value="This is a text node", children=[], props={"class": "text"})
@@ -70,6 +71,22 @@ class TestTextNode(unittest.TestCase):
         # dicts with same keys/values compare equal
         self.assertNotEqual(n1.props, n3.props)
         self.assertNotEqual(n1.props_to_html(), n3.props_to_html())
+
+    #LeafeNode tests
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://www.google.com">Click me!</a>',
+        )
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
 
 if __name__ == "__main__":
     unittest.main()
